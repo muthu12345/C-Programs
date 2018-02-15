7e9eebc114e9df0be9770b099cfae687b39b3629
muthu12345 <36498222+muthu12345@users.noreply.github.com>
2018-02-15 14:13:42 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+//Adding two integers
+#include<stdio.h>
+int main()
+{
+  int a, b, res;
+  printf("\nEnter the two integers:");
+  scanf("%d %d",&a, &b);
+  res = a + b;
+  printf("\nThe sum is = %d",res);
+  return(0);
+}
